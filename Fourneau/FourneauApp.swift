1d7019138f7dd870b16fcb5e4a3124f921ee7ce4
wrhenders <wrhenders@gmail.com>
2022-11-28 09:48:17 -0600

    Initial Commit

---
+++ Fourneau/FourneauApp.swift
@@ -0,0 +1,17 @@
+//
+//  FourneauApp.swift
+//  Fourneau
+//
+//  Created by Ryan Henderson on 11/28/22.
+//
+
+import SwiftUI
+
+@main
+struct FourneauApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
