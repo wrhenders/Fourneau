b5da61a10df72704cea578b5a5c98820a3328c73
wrhenders <wrhenders@gmail.com>
2022-12-20 11:58:07 -0600

    Added View Grouping and Shop View

---
+++ Fourneau/Views/Shop/ShopCard.swift
@@ -0,0 +1,20 @@
+//
+//  ShopCard.swift
+//  Fourneau
+//
+//  Created by Ryan Henderson on 12/20/22.
+//
+
+import SwiftUI
+
+struct ShopCard: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ShopCard_Previews: PreviewProvider {
+    static var previews: some View {
+        ShopCard()
+    }
+}
---
+++ Fourneau/Views/Shop/ShopView.swift
@@ -0,0 +1,20 @@
+//
+//  ShopView.swift
+//  Fourneau
+//
+//  Created by Ryan Henderson on 12/20/22.
+//
+
+import SwiftUI
+
+struct ShopView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ShopView_Previews: PreviewProvider {
+    static var previews: some View {
+        ShopView()
+    }
+}
